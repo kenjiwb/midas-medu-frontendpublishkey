31611ef802bd552e83d6158ce9837971c8c5bbb8
mahanthony <kenjiwb193@gmail.com>
2024-01-07 23:29:23 +0800

    initial commit

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,24 @@
+import { getCollectionsList } from "@lib/data"
+import FeaturedProducts from "@modules/home/components/featured-products"
+import Hero from "@modules/home/components/hero"
+import SkeletonHomepageProducts from "@modules/skeletons/components/skeleton-homepage-products"
+import { Metadata } from "next"
+import { Suspense } from "react"
+
+export const metadata: Metadata = {
+  title: "JPBS STORE",
+  description: "The ulitmate place for your own needs!",
+}
+
+export default async function Home() {
+  const { collections, count } = await getCollectionsList(0, 3)
+
+  return (
+    <>
+      <Hero />
+      <Suspense fallback={<SkeletonHomepageProducts count={count} />}>
+        <FeaturedProducts collections={collections} />
+      </Suspense>
+    </>
+  )
+}
---
+++ src/modules/home/components/hero/index.tsx
@@ -0,0 +1,59 @@
+import { Button, Heading } from "@medusajs/ui"
+import InteractiveLink from "@modules/common/components/interactive-link"
+import { Github } from "@medusajs/icons"
+import Head from "next/head"
+// import jpbs from "../../../../image/jpbs.jpeg"
+// const backgroundImageUrl = jpbs
+const styles = {
+  container: {
+    backgroundImage: `url(https://i.ibb.co/C6r1rMt/jpbs.jpg)`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  },
+}
+
+const Hero = () => {
+  if (typeof window !== "undefined") {
+    const viewportWidth = window.innerWidth
+    const isMobileSize = viewportWidth <= 768 // Adjust the threshold as needed
+    console.log(`Viewport Width: ${viewportWidth}`)
+    console.log(isMobileSize ? "Mobile Size" : "Not Mobile Size")
+  }
+  return (
+    <div
+      style={styles.container}
+      className="w-screen h-screen flex justify-center md:bg-cover md:bg-center sm:h-[45vh] md:h-[45vh]"
+    >
+      <div>
+        {/* <span>
+          
+          <Heading
+            level="h1"
+            className="text-3xl leading-10 text-ui-fg-base font-normal"
+          >
+            Ecommerce Starter Template
+          </Heading>
+          <Heading
+            level="h2"
+            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+          >
+            Powered by Medusa and Next.js
+          </Heading>
+        </span>
+        <a
+          href="https://github.com/medusajs/nextjs-starter-medusa"
+          target="_blank"
+        >
+          <Button variant="secondary">
+            View on GitHub
+            <Github />
+          </Button>
+        </a> */}
+        {/* <img src="https://i.ibb.co/C6r1rMt/jpbs.jpg" alt="jpbs" /> */}
+        {/* <div style={styles.container}></div> */}
+      </div>
+    </div>
+  )
+}
+
+export default Hero
