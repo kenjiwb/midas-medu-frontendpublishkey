1963c29cc30e6a1cdb730d73ab868cb872dcda39
mahanthony <kenjiwb193@gmail.com>
2024-01-16 07:10:04 +0800

    update design

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,57 +1,29 @@
-import { Button, Heading } from "@medusajs/ui"
-import InteractiveLink from "@modules/common/components/interactive-link"
-import { Github } from "@medusajs/icons"
-import Head from "next/head"
-// import Midas Lifestyle from "../../../../image/Midas Lifestyle.jpeg"
-// const backgroundImageUrl = Midas Lifestyle
-const styles = {
-  container: {
-    backgroundImage: `url(https://i.ibb.co/C6r1rMt/Midas Lifestyle.jpg)`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-  },
-}
+import UnderlineLink from "@modules/common/components/underline-link"
+import Image from "next/image"
 
 const Hero = () => {
-  if (typeof window !== "undefined") {
-    const viewportWidth = window.innerWidth
-    const isMobileSize = viewportWidth <= 768 // Adjust the threshold as needed
-    console.log(`Viewport Width: ${viewportWidth}`)
-    console.log(isMobileSize ? "Mobile Size" : "Not Mobile Size")
-  }
   return (
-    <div
-      style={styles.container}
-      className="w-screen h-screen flex justify-center md:bg-cover md:bg-center sm:h-[45vh] md:h-[45vh]"
-    >
-      <div>
-        {/* <span>
-          
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Ecommerce Starter Template
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Powered by Medusa and Next.js
-          </Heading>
-        </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
-        </a> */}
-        {/* <img src="https://i.ibb.co/C6r1rMt/Midas Lifestyle.jpg" alt="Midas Lifestyle" /> */}
-        {/* <div style={styles.container}></div> */}
+    <div className="h-[90vh] w-full relative">
+      <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
+          Inspire Your Dream Home
+        </h1>
+        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
+          A Midas Touch To Your Lifestyle
+        </p>
+        <UnderlineLink href="/">Store</UnderlineLink>
       </div>
+      <Image
+        src="/hero.jpg"
+        layout="fill"
+        loading="eager"
+        priority={true}
+        quality={90}
+        objectFit="cover"
+        alt="homepage banner"
+        className="absolute inset-0"
+        draggable="false"
+      />
     </div>
   )
 }
