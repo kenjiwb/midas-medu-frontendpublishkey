afe1fca8ae268751ae0e8d46dd15c7f4450b6b9d
youngdevs420 <143888474+youngdevs420@users.noreply.github.com>
2024-01-21 04:02:22 -0800

    header logo added (#1)

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -20,7 +20,7 @@ const Nav = () => {
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
-      <header className="relative h-16 px-8 mx-auto border-b duration-200 bg-white border-ui-border-base">
+      <header className="relative h-20 px-8 mx-auto border-b duration-200 bg-white border-ui-border-base">
         <nav className="txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="block small:hidden">
@@ -36,7 +36,7 @@ const Nav = () => {
               href="/"
               className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
             >
-              Midas Lifestyle Store
+              <img src="https://static.wixstatic.com/media/9ef40d_58a1a80b9a9641d69c61436c550136cb~mv2.png/v1/fill/w_68,h_68,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/logo.png" height="60" width="60" alt="" />
             </Link>
           </div>
 
